576e8b9d5cd13c49d91a114fb07c6131243964db
anton.beloglazov@gmail.com <anton.beloglazov@gmail.com@4eb84f4b-ba4d-25d6-b9a1-b968c9957e3e>
2012-01-05 05:48:31 +0000

    Fixed the examples of the static threshold policy with a random workload

--- trunk/modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/power/random/NonPowerAware.java
+++ trunk/modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/power/random/NonPowerAware.java
@@ -18,6 +18,19 @@ import org.cloudbus.cloudsim.power.PowerVmAllocationPolicySimple;
 /**
  * A simulation of a heterogeneous non-power aware data center: all hosts consume maximum power all
  * the time.
+ * 
+ * The remaining configuration parameters are in the Constants and RandomConstants classes.
+ * 
+ * If you are using any algorithms, policies or workload included in the power package please cite
+ * the following paper:
+ * 
+ * Anton Beloglazov, and Rajkumar Buyya, "Optimal Online Deterministic Algorithms and Adaptive
+ * Heuristics for Energy and Performance Efficient Dynamic Consolidation of Virtual Machines in
+ * Cloud Data Centers", Concurrency and Computation: Practice and Experience, ISSN: 1532-0626, Wiley
+ * Press, New York, USA, 2011, DOI: 10.1002/cpe.1867
+ * 
+ * @author Anton Beloglazov
+ * @since Jan 5, 2012
  */
 public class NonPowerAware {
 
@@ -28,7 +41,7 @@ public class NonPowerAware {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		String experimentName = "npa";
+		String experimentName = "random_npa";
 		String outputFolder = "output";
 
 		Log.setDisabled(!Constants.ENABLE_OUTPUT);
