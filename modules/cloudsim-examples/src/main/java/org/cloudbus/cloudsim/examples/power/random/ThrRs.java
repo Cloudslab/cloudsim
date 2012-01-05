6645832bbf4b0e896f6e6464b419d299675585ff
Anton Beloglazov <beloglazov@users.noreply.github.com>
2012-01-05 05:48:31 +0000

    Fixed the examples of the static threshold policy with a random workload

--- modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/power/random/ThrRs.java
+++ modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/power/random/ThrRs.java
@@ -3,9 +3,8 @@ package org.cloudbus.cloudsim.examples.power.random;
 import java.io.IOException;
 
 /**
- * A simulation of a heterogeneous power aware data center that only applied DVFS, but no dynamic
- * optimization of the VM allocation. The adjustment of the hosts' power consumption according to
- * their CPU utilization is happening in the PowerDatacenter class.
+ * A simulation of a heterogeneous power aware data center that applies the Static Threshold (THR)
+ * VM allocation policy and Random Selection (RS) VM selection policy.
  * 
  * The remaining configuration parameters are in the Constants and RandomConstants classes.
  * 
@@ -20,21 +19,9 @@ import java.io.IOException;
  * @author Anton Beloglazov
  * @since Jan 5, 2012
  */
-public class Dvfs extends RandomRunnerAbstract {
+public class ThrRs extends RandomRunnerAbstract {
 
-	/**
-	 * Instantiates a new dvfs.
-	 * 
-	 * @param enableOutput the enable output
-	 * @param outputToFile the output to file
-	 * @param inputFolder the input folder
-	 * @param outputFolder the output folder
-	 * @param workload the workload
-	 * @param vmAllocationPolicy the vm allocation policy
-	 * @param vmSelectionPolicy the vm selection policy
-	 * @param parameter the parameter
-	 */
-	public Dvfs(
+	public ThrRs(
 			boolean enableOutput,
 			boolean outputToFile,
 			String inputFolder,
@@ -54,21 +41,15 @@ public class Dvfs extends RandomRunnerAbstract {
 				parameter);
 	}
 
-	/**
-	 * The main method.
-	 * 
-	 * @param args the arguments
-	 * @throws IOException Signals that an I/O exception has occurred.
-	 */
 	public static void main(String[] args) throws IOException {
 		boolean enableOutput = true;
 		boolean outputToFile = false;
 		String inputFolder = "";
 		String outputFolder = "";
 		String workload = "random"; // Random workload
-		String vmAllocationPolicy = "dvfs"; // DVFS policy without VM migrations
-		String vmSelectionPolicy = "";
-		String parameter = "";
+		String vmAllocationPolicy = "thr"; // Static Threshold VM allocation policy
+		String vmSelectionPolicy = "rs"; // Random Selection VM selection policy
+		String parameter = "0.8"; // the static utilization threshold
 
 		new Dvfs(
 				enableOutput,
