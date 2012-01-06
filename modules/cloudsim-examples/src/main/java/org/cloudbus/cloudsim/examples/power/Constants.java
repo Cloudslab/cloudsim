863a9c9313a7549d17119fab2e29b68d0e1a4e61
Anton Beloglazov <beloglazov@users.noreply.github.com>
2012-01-06 08:26:36 +0000

    Updated the javadocs

--- modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/power/Constants.java
+++ modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/power/Constants.java
@@ -4,6 +4,18 @@ import org.cloudbus.cloudsim.power.models.PowerModel;
 import org.cloudbus.cloudsim.power.models.PowerModelSpecPowerHpProLiantMl110G4Xeon3040;
 import org.cloudbus.cloudsim.power.models.PowerModelSpecPowerHpProLiantMl110G5Xeon3075;
 
+/**
+ * If you are using any algorithms, policies or workload included in the power package, please cite
+ * the following paper:
+ * 
+ * Anton Beloglazov, and Rajkumar Buyya, "Optimal Online Deterministic Algorithms and Adaptive
+ * Heuristics for Energy and Performance Efficient Dynamic Consolidation of Virtual Machines in
+ * Cloud Data Centers", Concurrency and Computation: Practice and Experience, ISSN: 1532-0626, Wiley
+ * Press, New York, USA, 2011, DOI: 10.1002/cpe.1867
+ * 
+ * @author Anton Beloglazov - IBM R&D Australia
+ * @since Jan 6, 2012
+ */
 public class Constants {
 
 	public final static boolean ENABLE_OUTPUT = true;
