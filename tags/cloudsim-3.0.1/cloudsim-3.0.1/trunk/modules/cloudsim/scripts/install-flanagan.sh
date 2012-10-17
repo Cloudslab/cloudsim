79399c11fb4133d52a881ab73f94db06ac9db951
nikolay.grozev@gmail.com <nikolay.grozev@gmail.com@4eb84f4b-ba4d-25d6-b9a1-b968c9957e3e>
2012-10-17 06:45:13 +0000

    removing erronous tag

--- tags/cloudsim-3.0.1/cloudsim-3.0.1/trunk/modules/cloudsim/scripts/install-flanagan.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wget http://www.ee.ucl.ac.uk/~mflanaga/java/flanagan.jar
-mvn install:install-file -DgroupId=org.flanagan -DartifactId=flanagan -Dversion=1.0 -Dfile=flanagan.jar -Dpackaging=jar -DgeneratePom=true
-rm flanagan.jar
