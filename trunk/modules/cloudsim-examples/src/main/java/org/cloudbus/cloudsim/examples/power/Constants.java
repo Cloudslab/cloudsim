89d058a788ef8cfaa6dd3211bfabfcc355b2760b
anton.beloglazov@gmail.com <anton.beloglazov@gmail.com@4eb84f4b-ba4d-25d6-b9a1-b968c9957e3e>
2012-10-28 23:56:46 +0000

    Fixed an author annotation

--- trunk/modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/power/Constants.java
+++ trunk/modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/power/Constants.java
@@ -7,13 +7,13 @@ import org.cloudbus.cloudsim.power.models.PowerModelSpecPowerHpProLiantMl110G5Xe
 /**
  * If you are using any algorithms, policies or workload included in the power package, please cite
  * the following paper:
- * 
+ *
  * Anton Beloglazov, and Rajkumar Buyya, "Optimal Online Deterministic Algorithms and Adaptive
  * Heuristics for Energy and Performance Efficient Dynamic Consolidation of Virtual Machines in
  * Cloud Data Centers", Concurrency and Computation: Practice and Experience (CCPE), Volume 24,
  * Issue 13, Pages: 1397-1420, John Wiley & Sons, Ltd, New York, USA, 2012
- * 
- * @author Anton Beloglazov - IBM R&D Australia
+ *
+ * @author Anton Beloglazov
  * @since Jan 6, 2012
  */
 public class Constants {
@@ -25,8 +25,8 @@ public class Constants {
 	public final static double SIMULATION_LIMIT = 24 * 60 * 60;
 
 	public final static int CLOUDLET_LENGTH	= 2500 * (int) SIMULATION_LIMIT;
-	public final static int CLOUDLET_PES	= 1; 
- 
+	public final static int CLOUDLET_PES	= 1;
+
 	/*
 	 * VM instance types:
 	 *   High-Memory Extra Large Instance: 3.25 EC2 Compute Units, 8.55 GB // too much MIPS
