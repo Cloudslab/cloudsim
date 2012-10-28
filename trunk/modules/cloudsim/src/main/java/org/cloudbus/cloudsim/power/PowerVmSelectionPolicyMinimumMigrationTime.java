e72522430c0a8eac798a596e650af9fe376842c8
anton.beloglazov@gmail.com <anton.beloglazov@gmail.com@4eb84f4b-ba4d-25d6-b9a1-b968c9957e3e>
2012-10-28 23:52:39 +0000

    Updated the reference to the CCPE paper

--- trunk/modules/cloudsim/src/main/java/org/cloudbus/cloudsim/power/PowerVmSelectionPolicyMinimumMigrationTime.java
+++ trunk/modules/cloudsim/src/main/java/org/cloudbus/cloudsim/power/PowerVmSelectionPolicyMinimumMigrationTime.java
@@ -20,8 +20,8 @@ import org.cloudbus.cloudsim.Vm;
  * 
  * Anton Beloglazov, and Rajkumar Buyya, "Optimal Online Deterministic Algorithms and Adaptive
  * Heuristics for Energy and Performance Efficient Dynamic Consolidation of Virtual Machines in
- * Cloud Data Centers", Concurrency and Computation: Practice and Experience, ISSN: 1532-0626, Wiley
- * Press, New York, USA, 2011, DOI: 10.1002/cpe.1867
+ * Cloud Data Centers", Concurrency and Computation: Practice and Experience (CCPE), Volume 24,
+ * Issue 13, Pages: 1397-1420, John Wiley & Sons, Ltd, New York, USA, 2012
  * 
  * @author Anton Beloglazov
  * @since CloudSim Toolkit 3.0
