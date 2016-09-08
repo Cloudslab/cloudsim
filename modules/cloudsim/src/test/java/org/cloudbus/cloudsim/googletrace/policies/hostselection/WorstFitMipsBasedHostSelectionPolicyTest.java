6b3cb813c203578fc46e040cdfc9765d2c7ab7ba
giovannifs <giovanni@lsd.ufcg.edu.br>
2016-09-08 11:54:17 -0300

    Adding WorstFitMipsBasedHostSelectionPolicyTest class

---
+++ modules/cloudsim/src/test/java/org/cloudbus/cloudsim/googletrace/policies/hostselection/WorstFitMipsBasedHostSelectionPolicyTest.java
@@ -0,0 +1,14 @@
+package org.cloudbus.cloudsim.googletrace.policies.hostselection;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WorstFitMipsBasedHostSelectionPolicyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
