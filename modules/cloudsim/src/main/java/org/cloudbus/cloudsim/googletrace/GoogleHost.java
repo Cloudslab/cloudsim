f058ad5a0fbc630806c0ee51cf13b5c855d426a8
alessandroliafook <alessandro.liafook@gmail.com>
2016-09-09 11:51:56 -0300

    Adjusting compareTo method()

--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/googletrace/GoogleHost.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/googletrace/GoogleHost.java
@@ -8,21 +8,31 @@ import org.cloudbus.cloudsim.VmScheduler;
 import org.cloudbus.cloudsim.provisioners.BwProvisionerSimple;
 import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
 
-public class GoogleHost extends Host implements Comparable<Host>{
+public class GoogleHost extends Host implements Comparable<Host> {
 
-	public GoogleHost(int id, List<? extends Pe> peList, VmScheduler vmScheduler) {
-		super(id, new RamProvisionerSimple(Integer.MAX_VALUE),
-				new BwProvisionerSimple(Integer.MAX_VALUE), Integer.MAX_VALUE,
-				peList, vmScheduler);
-	}
+    public GoogleHost(int id, List<? extends Pe> peList, VmScheduler vmScheduler) {
+        super(id, new RamProvisionerSimple(Integer.MAX_VALUE),
+                new BwProvisionerSimple(Integer.MAX_VALUE), Integer.MAX_VALUE,
+                peList, vmScheduler);
+    }
 
-	@Override
-	public int compareTo(Host other) {
-		/*
+    @Override
+    public int compareTo(Host other) {
+        /*
 		 * If this object has bigger amount of available mips it should be
 		 * considered before the other one.
 		 */
-		return (-1)	* (new Double(getAvailableMips()).compareTo(new Double(other
-						.getAvailableMips())));
-	}
+        int result = (-1) * (new Double(getAvailableMips()).compareTo(new Double(other
+                .getAvailableMips())));
+
+        if (result == 0)
+            return new Integer(getId()).compareTo(new Integer(other.getId()));
+
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId();
+    }
 }
