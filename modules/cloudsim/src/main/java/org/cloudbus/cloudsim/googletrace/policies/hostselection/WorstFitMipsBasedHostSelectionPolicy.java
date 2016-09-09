fbc5df9fbddbabfcadd00c45df56da79164db28a
alessandroliafook <alessandro.liafook@gmail.com>
2016-09-09 10:44:11 -0300

    Implementing IllegalArgumentException

--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/googletrace/policies/hostselection/WorstFitMipsBasedHostSelectionPolicy.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/googletrace/policies/hostselection/WorstFitMipsBasedHostSelectionPolicy.java
@@ -1,5 +1,6 @@
 package org.cloudbus.cloudsim.googletrace.policies.hostselection;
 
+import java.util.NoSuchElementException;
 import java.util.SortedSet;
 
 import org.cloudbus.cloudsim.Host;
@@ -7,25 +8,34 @@ import org.cloudbus.cloudsim.Vm;
 
 /**
  * TODO
- * 
- * @author Giovanni Farias
  *
+ * @author Giovanni Farias
  */
 public class WorstFitMipsBasedHostSelectionPolicy implements HostSelectionPolicy {
 
-	@Override
-	public Host select(SortedSet<Host> hosts, Vm vm) {
+    @Override
+    public Host select(SortedSet<Host> hosts, Vm vm) {
 
-		double requiredMips = vm.getMips();
+        if (hosts == null)
+            throw new IllegalArgumentException("The set of host can not be null.");
+
+        if (vm == null)
+            throw new IllegalArgumentException("The Vm can not be null.");
+
+        double requiredMips = vm.getMips();
 
 		/*
-		 * Only first host is checked because the hosts are sorted from bigger
+         * Only first host is checked because the hosts are sorted from bigger
 		 * to smaller available capacity
 		 */
-		Host firstHost = hosts.first();
-		if (firstHost.getAvailableMips() >= requiredMips) {
-			return firstHost;
-		}
-		return null;
-	}
+        if (!hosts.isEmpty()) {
+
+            Host firstHost = hosts.first();
+
+            if (firstHost.getAvailableMips() >= requiredMips) {
+                return firstHost;
+            }
+        }
+        return null;
+    }
 }
