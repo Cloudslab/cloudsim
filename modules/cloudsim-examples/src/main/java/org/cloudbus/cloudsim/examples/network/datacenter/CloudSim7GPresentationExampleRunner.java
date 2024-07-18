49bc7e436060b04b762267a39d0ba0d66ec3def7
Remo <andreoliremo@gmail.com>
2024-07-18 13:07:55 +1000

    Added a variation of TandemAppExample5CloudSim7GPaper.java (that was presented in clouds lab)

---
+++ modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/network/datacenter/CloudSim7GPresentationExampleRunner.java
@@ -0,0 +1,77 @@
+package org.cloudbus.cloudsim.examples.network.datacenter;
+
+import org.cloudbus.cloudsim.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CloudSim7GPresentationExampleRunner {
+    private static String testName = "";
+
+    // i) $T0$ and $T1$ are co-located, meaning that data is transmitted locally without using the physical network
+    private static Map<String, Integer> getConfigI() {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("cla", 0);
+        map.put("clb", 0);
+
+        return map;
+    }
+
+    // ii) $T0$ and $T1$ are located on the same rack, meaning that data is routed through a ToR switch only
+    private static Map<String, Integer> getConfigII() {
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("cla", 0);
+        map.put("clb", 1);
+
+        return map;
+    }
+
+    // iii) $TO$ and $T1$ are located on different racks, so that data must be transmitted through the aggregate switch as well
+    private static Map<String, Integer> getConfigIII() {
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("cla", 0);
+        map.put("clb", 2);
+
+        return map;
+    }
+
+    public static void main(String[] args) {
+        String[] emptyArgs = new String[0];
+        Log.setDisabled(true);
+
+        TandemAppExample5CloudSim7GPresentation.numberOfPeriodicActivations = 1;
+
+        // name->(cloudlet->vm) map
+        Map<String, Map<String, Integer>> configs = new HashMap<>();
+        configs.put("I", getConfigI());
+        configs.put("II", getConfigII());
+        configs.put("III", getConfigIII());
+
+        // name -> payload-size map
+        Map<String, Integer> vals = new HashMap<>();
+        vals.put("small-payload", 1);
+        vals.put("big-payload", 1000000000);
+
+        if (TandemAppExample5CloudSim7GPresentation.numberOfPeriodicActivations > 1) {
+            testName = "many";
+        } else {
+            testName = "single";
+        }
+
+        // Runner loop
+        for (var config : configs.entrySet()) {
+            TandemAppExample5CloudSim7GPresentation.cloudletToHost = config.getValue();
+
+            for (var val : vals.entrySet()) {
+                System.out.println("Running '"+testName+"' example with payload "+val.getValue()+" and config "+config.getKey());
+
+                TandemAppExample5CloudSim7GPresentation.payloadSize = val.getValue();
+                TandemAppExample5CloudSim7GPresentation.fileInfo = "-"+ val.getKey() +"-"+ config.getKey() +"-"+ testName;
+                TandemAppExample5CloudSim7GPresentation.main(emptyArgs);
+            }
+        }
+    }
+}
---
+++ modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/network/datacenter/TandemAppExample5CloudSim7GPresentation.java
@@ -0,0 +1,364 @@
+package org.cloudbus.cloudsim.examples.network.datacenter;
+
+import org.cloudbus.cloudsim.*;
+import org.cloudbus.cloudsim.EX.DatacenterBrokerEX;
+import org.cloudbus.cloudsim.container.utils.CustomCSVWriter;
+import org.cloudbus.cloudsim.core.CloudSim;
+import org.cloudbus.cloudsim.core.GuestEntity;
+import org.cloudbus.cloudsim.distributions.ExponentialDistr;
+import org.cloudbus.cloudsim.network.datacenter.*;
+import org.cloudbus.cloudsim.provisioners.BwProvisionerSimple;
+import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple;
+import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
+import org.cloudbus.cloudsim.selectionPolicies.SelectionPolicyLeastFull;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class TandemAppExample5CloudSim7GPresentation {
+
+	private static List<GuestEntity> guestList;
+
+	private static List<NetworkHost> hostList;
+
+    private static List<AppCloudlet> appCloudletList;
+
+	private static DatacenterBrokerEX broker;
+
+	public static final int numberOfHosts = 4;
+	public static final int numberOfVms = 4;
+	public static int numberOfPeriodicActivations = 20;
+
+
+	public static int firstTaskExecLength = 10000000;
+	public static int secondTaskExecLength = 10000000;
+	public static int payloadSize = 1000000000;
+
+	private static ExponentialDistr distr;
+
+	public static int deadline = 2000;
+
+	public static long vmMips = 10000;
+	public static long vmBw = 1024 * 1024 * 1024; // 1 Gbps
+
+	public static long hostToSwitchBw = 1024 * 1024 * 1024; // 1 Gbps
+	public static long switchToSwitchBw = 1024 * 1024 * 1024;
+
+	public static Map<String, Integer> cloudletToHost;
+
+	public static String fileInfo = "";
+
+	/**
+	 *
+	 * A customised version of TandemAppExample5 for the CloudSim7G presentation.
+	 *
+	 * Example of periodic tandem DAG: A ---> B
+	 * with Datacenter configuration:
+*                                                Agg. Switch
+* 										         /        \
+* 										ToR switch        ToR switch
+* 								   		 /	 \  		   /	 \
+* 									 Host0    Host1     Host2    Host3
+	 * 						   		  VM0      VM1       VM2      VM3
+	 *
+	 *
+	 * Depending on the cloudlet placement, the network may be used or not.
+	 * 
+	 * @param args the args
+	 * @author Remo Andreoli
+	 */
+	public static void main(String[] args) {
+		distr = new ExponentialDistr(5, (double) firstTaskExecLength/vmMips + (double) secondTaskExecLength/vmMips);
+
+		Log.println("Starting TandemAppExample5...");
+
+		try {
+			int num_user = 1; // number of cloud users
+			Calendar calendar = Calendar.getInstance();
+			boolean trace_flag = false; // mean trace events
+
+			// Initialize the CloudSim library
+			CloudSim.init(num_user, calendar, trace_flag);
+
+			hostList = CreateHosts();
+
+			// Third step: Create Broker (Make sure the broker stays alive the whole time)
+			broker = new DatacenterBrokerEX("Broker", 1000000);
+
+			// Create vms
+			guestList = CreateVms();
+
+			appCloudletList = new ArrayList<>();
+
+			for (int i = 0; i < numberOfPeriodicActivations; i++) {
+				AppCloudlet app = new AppCloudlet(AppCloudlet.APP_Workflow, i, deadline, broker.getId());
+				createTaskList(app);
+				appCloudletList.add(app);
+
+				broker.submitCloudletList(app.cList, distr.sample());
+			}
+
+			// Datacenters are the resource providers in CloudSim. We need at
+			// list one of them to run a CloudSim simulation
+			createDatacenter("Datacenter_0");
+
+			// submit vm list to the broker
+			broker.submitGuestList(guestList);
+
+			// Sixth step: Starts the simulation
+			CloudSim.startSimulation();
+
+			CloudSim.stopSimulation();
+
+			// Final step: Print results when simulation is over
+			List<Cloudlet> newList = broker.getCloudletReceivedList();
+			printCloudletList(newList);
+			System.out.println("numberofcloudlet " + newList.size() + " Data transfered "
+					+ NetworkGlobals.totaldatatransfer);
+
+			Log.println("TandemAppExample5 finished!");
+
+			/*for (AppCloudlet app : appCloudletList) {
+				for (NetworkCloudlet ncl : app.cList) {
+					ncl.stats();
+				}
+			}*/
+
+			String paddedD = String.format("%04d", deadline);
+			String paddedP = String.format("%04d", deadline);
+			CustomCSVWriter writer = new CustomCSVWriter("/tmp/D"+paddedD+"-P"+paddedP+fileInfo+".csv");
+
+			String[] header = {"appId", "startTime", "endTime", "E2E", "period", "lateness", "noise"};
+			if (!writer.fileExistedAlready()) {
+				writer.writeTofile(header, false);
+			}
+
+			for (AppCloudlet app : appCloudletList) {
+				String[] data = new String[header.length];
+
+				data[0] = String.valueOf(app.appID);
+				data[1] = String.valueOf(app.cList.get(0).getExecStartTime());
+				data[2] = String.valueOf(app.cList.get(1).getExecFinishTime());
+				data[3] = String.valueOf(deadline);
+				data[4] = String.valueOf(deadline);
+				data[5] = String.valueOf(app.getLateness());
+				data[6] = "False";
+				writer.writeTofile(data, true);
+			}
+
+			for (AppCloudlet app : appCloudletList) {
+				Log.println("App #"+ app.appID + " " +(app.cList.get(1).getExecFinishTime() - app.cList.get(0).getExecStartTime()));
+			}
+
+			System.out.println("Result in "+writer.getFileAddress());
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.println("Unwanted errors happen");
+		}
+	}
+
+	/**
+	 * Creates the datacenter.
+	 * 
+	 * @param name
+	 *            the name
+	 * 
+	 * @return the datacenter
+	 */
+	private static NetworkDatacenter createDatacenter(String name) {
+		String arch = "x86"; // system architecture
+		String os = "Linux"; // operating system
+		String vmm = "Xen";
+		double time_zone = 10.0; // time zone this resource located
+		double cost = 3.0; // the cost of using processing in this resource
+		double costPerMem = 0.05; // the cost of using memory in this resource
+		double costPerStorage = 0.001; // the cost of using storage in this resource
+		double costPerBw = 0.0; // the cost of using bw in this resource
+		LinkedList<Storage> storageList = new LinkedList<>(); // no SAN devices as of now
+
+		DatacenterCharacteristics characteristics = new DatacenterCharacteristics(
+				arch,
+				os,
+				vmm,
+				hostList,
+				time_zone,
+				cost,
+				costPerMem,
+				costPerStorage,
+				costPerBw);
+
+		NetworkDatacenter datacenter = null;
+		try {
+			datacenter = new NetworkDatacenter(
+					name,
+					characteristics,
+					new VmAllocationWithSelectionPolicy(hostList, new SelectionPolicyLeastFull()),
+					storageList,
+					0);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Create Internal Datacenter network
+		CreateNetwork(datacenter);
+		return datacenter;
+	}
+
+	/**
+	 * Prints the Cloudlet objects.
+	 * 
+	 * @param list
+	 *            list of Cloudlets
+	 * @throws IOException
+	 */
+	private static void printCloudletList(List<Cloudlet> list) throws IOException {
+		String indent = "    ";
+		Log.println();
+		Log.println("========== OUTPUT ==========");
+		Log.println("Cloudlet ID" + indent + "STATUS" + indent + "Data center ID" + indent + "VM ID"
+				+ indent + "Time" + indent + "Start Time" + indent + "Finish Time");
+
+		DecimalFormat dft = new DecimalFormat("###.##");
+		for (Cloudlet cloudlet : list) {
+			Log.print(indent + cloudlet.getCloudletId() + indent + indent);
+
+			if (cloudlet.getStatus() == Cloudlet.CloudletStatus.SUCCESS) {
+				Log.print("SUCCESS");
+				Log.println(indent + indent + cloudlet.getResourceId() + indent + indent + indent
+						+ cloudlet.getGuestId() + indent + indent + dft.format(cloudlet.getActualCPUTime())
+						+ indent + indent + dft.format(cloudlet.getExecStartTime()) + indent + indent
+						+ dft.format(cloudlet.getExecFinishTime()));
+			}
+		}
+
+	}
+
+	private static List<NetworkHost> CreateHosts() {
+		int mips = 100000;
+		int ram = 100000; // host memory (MB)
+		long storage = 1000000; // host storage
+		long bw = 10L * 1024 * 1024 * 1024; // 10 Gbps
+
+		List<NetworkHost> hostList = new ArrayList<>();
+		for (int i=0; i<numberOfHosts; i++) {
+			List<Pe> peList = new ArrayList<>();
+			peList.add(new Pe(0, new PeProvisionerSimple(mips)));
+			peList.add(new Pe(1, new PeProvisionerSimple(mips)));
+			hostList.add(new NetworkHost(
+					i,
+					new RamProvisionerSimple(ram),
+					new BwProvisionerSimple(bw),
+					storage,
+					peList,
+					new VmSchedulerTimeShared(peList)));
+		}
+
+		return hostList;
+	}
+	/**
+	 * Creates virtual machines in a datacenter
+	 */
+	private static List<GuestEntity> CreateVms() {
+		ArrayList<GuestEntity> vmList = new ArrayList<>();
+
+		long size = 2048; // image size (MB)
+		int ram = 4096; // vm memory (MB)
+		int pesNumber = 1;
+		String vmm = "Xen";
+
+		for (int i=0; i<numberOfVms; i++) {
+			vmList.add(new Vm(
+					i,
+					broker.getId(),
+					vmMips,
+					pesNumber,
+					ram,
+					vmBw,
+					size,
+					vmm,
+					new CloudletSchedulerTimeShared()));
+		}
+
+		return vmList;
+	}
+
+	static private void createTaskList(AppCloudlet appCloudlet) {
+		long fileSize = NetworkConstants.FILE_SIZE;
+		long outputSize = NetworkConstants.OUTPUT_SIZE;
+		UtilizationModel utilizationModel = new UtilizationModelFull();
+
+		// Define cloudlets
+		NetworkCloudlet cla = new NetworkCloudlet(
+				NetworkConstants.currentCloudletId,
+				0,
+				1,
+				fileSize,
+				outputSize,
+				utilizationModel,
+				utilizationModel,
+				utilizationModel);
+		NetworkConstants.currentCloudletId++;
+		cla.setUserId(broker.getId());
+
+		if (cloudletToHost != null && cloudletToHost.containsKey("cla")) {
+			cla.setGuestId(cloudletToHost.get("cla"));
+		} else {
+			cla.setGuestId(guestList.get(0).getId());
+		}
+		appCloudlet.cList.add(cla);
+
+		NetworkCloudlet clb = new NetworkCloudlet(
+				NetworkConstants.currentCloudletId,
+				0,
+				1,
+				fileSize,
+				outputSize,
+				utilizationModel,
+				utilizationModel,
+				utilizationModel);
+		NetworkConstants.currentCloudletId++;
+		clb.setUserId(broker.getId());
+		if (cloudletToHost != null && cloudletToHost.containsKey("clb")) {
+			clb.setGuestId(cloudletToHost.get("clb"));
+		} else {
+			clb.setGuestId(guestList.get(1).getId());
+		}
+		appCloudlet.cList.add(clb);
+
+		// Configure task stages within the cloudlets
+		//
+		cla.addExecutionStage(firstTaskExecLength);
+		cla.addSendStage(payloadSize, clb);
+
+		//
+		clb.addRecvStage(cla);
+		clb.addExecutionStage(secondTaskExecLength);
+	}
+
+	private static void CreateNetwork(NetworkDatacenter dc) {
+		Switch ToRSwitch1 = new Switch("Edge0", NetworkConstants.EdgeSwitchPort, Switch.SwitchLevel.EDGE_LEVEL,
+					0, hostToSwitchBw, switchToSwitchBw, dc);
+		Switch ToRSwitch2 = new Switch("Edge1", NetworkConstants.EdgeSwitchPort, Switch.SwitchLevel.EDGE_LEVEL,
+				0, hostToSwitchBw, switchToSwitchBw, dc);
+
+		Switch AggrSwitch = new Switch("Aggr0", NetworkConstants.AggSwitchPort, Switch.SwitchLevel.AGGR_LEVEL,
+				0, switchToSwitchBw, switchToSwitchBw, dc);
+
+		dc.registerSwitch(ToRSwitch1);
+		dc.registerSwitch(ToRSwitch2);
+		dc.registerSwitch(AggrSwitch);
+
+		dc.attachSwitchToSwitch(ToRSwitch1, AggrSwitch);
+		dc.attachSwitchToSwitch(ToRSwitch2, AggrSwitch);
+
+		// Attach to hosts
+		List<NetworkHost> netHosts = dc.getHostList();
+		dc.attachSwitchToHost(ToRSwitch1, netHosts.get(0));
+		dc.attachSwitchToHost(ToRSwitch1, netHosts.get(1));
+
+		dc.attachSwitchToHost(ToRSwitch2, netHosts.get(2));
+		dc.attachSwitchToHost(ToRSwitch2, netHosts.get(3));
+	}
+}
