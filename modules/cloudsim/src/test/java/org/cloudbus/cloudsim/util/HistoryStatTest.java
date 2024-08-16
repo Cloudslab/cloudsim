281855b62845f96f99941eb458d13c2615a77157
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2024-08-16 21:52:39 +0200

    HistoryStatTest - adding testExpunge() testing correctness of median on expunged old samples, plus code cleanup

--- modules/cloudsim/src/test/java/org/cloudbus/cloudsim/util/HistoryStatTest.java
+++ modules/cloudsim/src/test/java/org/cloudbus/cloudsim/util/HistoryStatTest.java
@@ -6,153 +6,59 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import org.cloudbus.cloudsim.CloudletScheduler;
-import org.cloudbus.cloudsim.VmStateHistoryEntry;
-import org.cloudbus.cloudsim.core.HostEntity;
-import org.cloudbus.cloudsim.core.PowerGuestEntity;
+import static org.cloudbus.cloudsim.core.PowerGuestEntity.HISTORY_LENGTH;
 import org.junit.Test;
+import org.junit.Before;
 
 public class HistoryStatTest {
+    HistoryStat stat;
+    List<Double> history;
     private void addUtilizationHistoryValue(List<Double> history, final double utilization) {
         history.addFirst(utilization);
-        if (history.size() > PowerGuestEntity.HISTORY_LENGTH) {
-            history.remove(PowerGuestEntity.HISTORY_LENGTH);
+        if (history.size() > HISTORY_LENGTH) {
+            history.remove(HISTORY_LENGTH);
         }
+    }
+
+    @Before
+    public void setUp() {
+        stat = new HistoryStat(HISTORY_LENGTH);
+        history = new ArrayList<>(HISTORY_LENGTH);
     }
 
     @Test
     public void testSimple() {
-        HistoryStat s = new HistoryStat(PowerGuestEntity.HISTORY_LENGTH);
-        assertEquals(0.0, s.getMedian(), 0);
-        s.offer(10.0);
-        assertEquals(10.0, s.getMedian(), 0);
-        s.offer(20.0);
-        assertEquals(15.0, s.getMedian(), 0);
-        s.offer(10.0);
-        assertEquals(10.0, s.getMedian(), 0);
+        assertEquals(0.0, stat.getMedian(), 0);
+        stat.offer(10.0);
+        assertEquals(10.0, stat.getMedian(), 0);
+        stat.offer(20.0);
+        assertEquals(15.0, stat.getMedian(), 0);
+        stat.offer(10.0);
+        assertEquals(10.0, stat.getMedian(), 0);
     }
+
+    @Test
+    public void testExpunge() {
+        for (double val = 0.0; val < 2 * HISTORY_LENGTH; val += 1.0) {
+            stat.offer(val);
+            addUtilizationHistoryValue(history, val);
+            assertEquals(stat.getMedian(), MathUtil.median(history), 1e-6);
+            if (stat.size() <= HISTORY_LENGTH-1)
+                assertEquals(val/2.0, stat.getMedian(), 1e-6);
+            else
+                assertEquals((val+val-(HISTORY_LENGTH-1))/2.0, stat.getMedian(), 1e-6);
+        }
+    }
+
     @Test
     public void testCompared() {
-        HistoryStat s = new HistoryStat(PowerGuestEntity.HISTORY_LENGTH);
-        PowerGuestEntity e = new PowerGuestEntity() {
-            @Override
-            public HistoryStat getUtilizationHistory() { return null; }
-            @Override
-            public double updateCloudletsProcessing(double currentTime, List<Double> mipsShare) {
-                throw new UnsupportedOperationException("Unimplemented method 'updateCloudletsProcessing'");
-            }
-            @Override
-            public List<Double> getCurrentRequestedMips() {
-                throw new UnsupportedOperationException("Unimplemented method 'getCurrentRequestedMips'");
-            }
-            @Override
-            public long getCurrentRequestedBw() {
-                throw new UnsupportedOperationException("Unimplemented method 'getCurrentRequestedBw'");
-            }
-            @Override
-            public int getCurrentRequestedRam() {
-                throw new UnsupportedOperationException("Unimplemented method 'getCurrentRequestedRam'");
-            }
-            @Override
-            public double getTotalUtilizationOfCpu(double time) {
-                throw new UnsupportedOperationException("Unimplemented method 'getTotalUtilizationOfCpu'");
-            }
-            @Override
-            public <T extends HostEntity> T getHost() {
-                throw new UnsupportedOperationException("Unimplemented method 'getHost'");
-            }
-            @Override
-            public long getBw() {
-                throw new UnsupportedOperationException("Unimplemented method 'getBw'");
-            }
-            @Override
-            public int getRam() {
-                throw new UnsupportedOperationException("Unimplemented method 'getRam'");
-            }
-            @Override
-            public double getMips() {
-                throw new UnsupportedOperationException("Unimplemented method 'getMips'");
-            }
-            @Override
-            public double getTotalUtilizationOfCpuMips(double time) {
-                throw new UnsupportedOperationException("Unimplemented method 'getTotalUtilizationOfCpuMips'");
-            }
-            @Override
-            public long getSize() {
-                throw new UnsupportedOperationException("Unimplemented method 'getSize'");
-            }
-            @Override
-            public int getId() {
-                throw new UnsupportedOperationException("Unimplemented method 'getId'");
-            }
-            @Override
-            public int getUserId() {
-                throw new UnsupportedOperationException("Unimplemented method 'getUserId'");
-            }
-            @Override
-            public String getUid() {
-                throw new UnsupportedOperationException("Unimplemented method 'getUid'");
-            }
-            @Override
-            public List<VmStateHistoryEntry> getStateHistory() {
-                throw new UnsupportedOperationException("Unimplemented method 'getStateHistory'");
-            }
-            @Override
-            public CloudletScheduler getCloudletScheduler() {
-                throw new UnsupportedOperationException("Unimplemented method 'getCloudletScheduler'");
-            }
-            @Override
-            public <T extends HostEntity> void setHost(T host) {
-                throw new UnsupportedOperationException("Unimplemented method 'setHost'");
-            }
-            @Override
-            public void setCurrentAllocatedRam(int currentAllocatedRam) {
-                throw new UnsupportedOperationException("Unimplemented method 'setCurrentAllocatedRam'");
-            }
-            @Override
-            public void setCurrentAllocatedBw(long currentAllocatedBw) {
-                throw new UnsupportedOperationException("Unimplemented method 'setCurrentAllocatedBw'");
-            }
-            @Override
-            public void setCurrentAllocatedMips(List<Double> mips) {
-                throw new UnsupportedOperationException("Unimplemented method 'setCurrentAllocatedMips'");
-            }
-            @Override
-            public void setInMigration(boolean inMigration) {
-                throw new UnsupportedOperationException("Unimplemented method 'setInMigration'");
-            }
-            @Override
-            public void setBeingInstantiated(boolean beingInstantiated) {
-                throw new UnsupportedOperationException("Unimplemented method 'setBeingInstantiated'");
-            }
-            @Override
-            public boolean isInMigration() {
-                throw new UnsupportedOperationException("Unimplemented method 'isInMigration'");
-            }
-            @Override
-            public int getNumberOfPes() {
-                throw new UnsupportedOperationException("Unimplemented method 'getNumberOfPes'");
-            }
-            @Override
-            public double getTotalMips() {
-                throw new UnsupportedOperationException("Unimplemented method 'getTotalMips'");
-            }
-            @Override
-            public boolean isBeingInstantiated() {
-                throw new UnsupportedOperationException("Unimplemented method 'isBeingInstantiated'");
-            };
-            
-        };
-
         Random gen = new Random();
-        List<Double> history = new ArrayList<>(PowerGuestEntity.HISTORY_LENGTH);
-
         for (int i = 0; i < 1000; i++) {
             double val = gen.nextDouble();
-            s.offer(val);
+            stat.offer(val);
             addUtilizationHistoryValue(history, val);
-            assertEquals(s.getMedian(), MathUtil.median(history), 1e-6);
-            assertEquals(s.getMean(), MathUtil.mean(history), 1e-6);
+            assertEquals(stat.getMedian(), MathUtil.median(history), 1e-6);
+            assertEquals(stat.getMean(), MathUtil.mean(history), 1e-6);
         }
 
         for (int step = 0; step < 100; step++) {
@@ -165,9 +71,9 @@ public class HistoryStatTest {
             double meanSum = 0.0;
             long t0 = System.nanoTime();
             for (int i = 0; i < N; i++) {
-                s.offer(samples[i]);
-                medianSum += s.getMedian();
-                meanSum += s.getMean();
+                stat.offer(samples[i]);
+                medianSum += stat.getMedian();
+                meanSum += stat.getMean();
             }
 
             double medianSum2 = 0.0;
