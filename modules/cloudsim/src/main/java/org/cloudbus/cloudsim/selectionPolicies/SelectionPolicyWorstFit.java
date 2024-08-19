9627797a641ea25762be3d4cbe9d53fe10954417
Remo <andreoliremo@gmail.com>
2024-08-19 10:47:49 -0500

    GPL update

--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/selectionPolicies/SelectionPolicyWorstFit.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/selectionPolicies/SelectionPolicyWorstFit.java
@@ -1,3 +1,10 @@
+/*
+ * Title: CloudSim Toolkit Description: CloudSim (Cloud Simulation) Toolkit for Modeling and
+ * Simulation of Clouds Licence: GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * Copyright (c) 2009-2024, The University of Melbourne, Australia
+ */
+
 package org.cloudbus.cloudsim.selectionPolicies;
 
 import org.cloudbus.cloudsim.core.HostEntity;
