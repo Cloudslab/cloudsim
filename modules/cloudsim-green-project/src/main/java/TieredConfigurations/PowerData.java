27d7b39bfada5b6b2c60b984cdeb81ada46810cd
Raoul <6421655+Raove@users.noreply.github.com>
2024-10-26 15:29:49 -0700

    Update PowerData.java

--- modules/cloudsim-green-project/src/main/java/TieredConfigurations/PowerData.java
+++ modules/cloudsim-green-project/src/main/java/TieredConfigurations/PowerData.java
@@ -1,13 +1,24 @@
 package TieredConfigurations;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.IOException;
+
 public class PowerData {
     private double fossilFreePercentage;
     private double renewablePercentage;
 
     // Constructor
-    public PowerData(double fossilFreePercentage, double renewablePercentage) {
-        this.fossilFreePercentage = fossilFreePercentage;
-        this.renewablePercentage = renewablePercentage;
+    public PowerData() throws IOException {
+        EnergyAPI.ElectricityMapsAPI electricityMapsAPI = new EnergyAPI.ElectricityMapsAPI("JtPlg2xeVfyLW");
+        // Parse the JSON data
+        JsonObject jsonObject = JsonParser.parseString(electricityMapsAPI.getPowerBreakdown("FR")).getAsJsonObject();
+
+        // Extract the fossilFreePercentage and renewablePercentage values
+        this.fossilFreePercentage = jsonObject.get("fossilFreePercentage").getAsDouble();
+        this.renewablePercentage = jsonObject.get("renewablePercentage").getAsDouble();
+        System.out.println("Fossil Free Percentage: " + this.fossilFreePercentage);
+        System.out.println("Renewable Percentage: " + this.renewablePercentage);
     }
 
     public double getFossilFreePercentage() {
