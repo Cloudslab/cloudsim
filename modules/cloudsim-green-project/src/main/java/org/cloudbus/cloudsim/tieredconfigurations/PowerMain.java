53a13037e6a74229f68c969426fde6013de444ea
Raoul <6421655+Raove@users.noreply.github.com>
2024-11-04 11:58:18 -0800

    Update PowerMain.java

--- modules/cloudsim-green-project/src/main/java/org/cloudbus/cloudsim/tieredconfigurations/PowerMain.java
+++ modules/cloudsim-green-project/src/main/java/org/cloudbus/cloudsim/tieredconfigurations/PowerMain.java
@@ -1,11 +1,9 @@
 package org.cloudbus.cloudsim.tieredconfigurations;
 
-import org.cloudbus.cloudsim.Cloudlet;
-import org.cloudbus.cloudsim.Datacenter;
-import org.cloudbus.cloudsim.DatacenterBroker;
-import org.cloudbus.cloudsim.Vm;
+import org.cloudbus.cloudsim.*;
 import org.cloudbus.cloudsim.core.CloudSim;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class PowerMain {
@@ -45,8 +43,8 @@ class PowerMain {
             }
 
             // Create VMs and Cloudlets
-            List<Vm> vmList = CloudSimExample.createVMs();
-            List<Cloudlet> cloudletList = CloudSimExample.createCloudlets();
+            List<Vm> vmList = createVMs();
+            List<Cloudlet> cloudletList = createCloudlets();
 
             // Submit VMs and Cloudlets to the broker
             broker.submitGuestList(vmList);
@@ -80,5 +78,49 @@ class PowerMain {
                         + cloudlet.getExecStartTime() + indent + indent + cloudlet.getFinishTime());
             }
         }
+    }
+
+    public static List<Vm> createVMs() {
+        int vmid = 0;
+        int numVMs = 4;
+        List<Vm> vmList = new ArrayList<>();
+
+        for (int i = 0; i < numVMs; i++) {
+            int mips = 1000;
+            long size = 10000; // Image size (MB)
+            int ram = 512; // RAM (MB)
+            long bw = 1000; // Bandwidth
+            int pesNumber = 1; // Number of CPUs
+            String vmm = "Xen"; // VMM name
+
+            // Create the VM
+            Vm vm = new Vm(vmid++, 0, mips, pesNumber, ram, bw, size, vmm, new CloudletSchedulerTimeShared());
+            vmList.add(vm);
+        }
+
+        return vmList;
+    }
+
+    public static List<Cloudlet> createCloudlets() {
+        int cloudletId = 0;
+        int numCloudlets = 10;
+        List<Cloudlet> cloudletList = new ArrayList<>();
+
+        // Cloudlet properties
+        long length = 40000;
+        long fileSize = 300;
+        long outputSize = 300;
+        int pesNumber = 1;
+
+        // Full utilization model for CPU, RAM, and bandwidth
+        UtilizationModel utilizationModel = new UtilizationModelFull();
+
+        for (int i = 0; i < numCloudlets; i++) {
+            Cloudlet cloudlet = new Cloudlet(cloudletId++, length, pesNumber, fileSize, outputSize, utilizationModel, utilizationModel, utilizationModel);
+            cloudlet.setUserId(0); // Set the user ID for the cloudlet
+            cloudletList.add(cloudlet);
+        }
+
+        return cloudletList;
     }
 }
