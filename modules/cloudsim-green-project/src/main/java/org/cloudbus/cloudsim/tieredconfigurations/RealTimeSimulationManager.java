01f4fbdf16a0df47c26bc7032437292c6aa03c81
Raoul <6421655+Raove@users.noreply.github.com>
2024-11-04 17:50:03 -0800

    Update RealTimeSimulationManager.java

--- modules/cloudsim-green-project/src/main/java/org/cloudbus/cloudsim/tieredconfigurations/RealTimeSimulationManager.java
+++ modules/cloudsim-green-project/src/main/java/org/cloudbus/cloudsim/tieredconfigurations/RealTimeSimulationManager.java
@@ -29,7 +29,7 @@ public class RealTimeSimulationManager {
 
             // Reselect the datacenter based on updated power data
             currentDatacenter = PowerMain.selectDatacenterBasedOnPowerData(powerData);
-            System.out.println("Updated Datacenter selection based on fossil-free percentage: " + fossilFreePercentage);
+            System.out.println("Updated Datacenter(" + getCurrentDatacenter().getName() + ") selection based on fossil-free percentage: " + fossilFreePercentage);
 
         } catch (Exception e) {
             e.printStackTrace();
