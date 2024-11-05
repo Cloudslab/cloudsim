a07f680054b1ba206837e7f4bcdb75d80d73f64f
Raoul <6421655+Raove@users.noreply.github.com>
2024-11-04 18:10:18 -0800

    Update RealTimeSimulationManager.java

--- modules/cloudsim-green-project/src/main/java/org/cloudbus/cloudsim/tieredconfigurations/RealTimeSimulationManager.java
+++ modules/cloudsim-green-project/src/main/java/org/cloudbus/cloudsim/tieredconfigurations/RealTimeSimulationManager.java
@@ -37,6 +37,14 @@ public class RealTimeSimulationManager {
         }
     }
 
+
+    /*
+    Example Scenario to possibly create here?
+    8:00 AM - High Resource Datacenter selected based on 80% renewable energy. All cloudlets are allocated to high-capacity VMs, with quick execution.
+    11:00 AM - Renewable energy drops to 60%. The Medium Resource Datacenter is selected. New cloudlets are scheduled on medium-capacity VMs. Maybe migrate old cloudlets to new VMs?
+    3:00 PM - Renewables fall below 30%. The Low Resource Datacenter is chosen. Same as before? Maybe limit cloudlets to only run important ones?
+    7:00 PM - Renewables increase back to 50%. The Medium Resource Datacenter is selected again, migrate to this again and allow more cloudlets run?
+     */
 //    private void simulatePowerDataChange() {
 //        // Simulate fossil-free percentage change for testing
 //        double newFossilFreePercentage = Math.random() * 100;
