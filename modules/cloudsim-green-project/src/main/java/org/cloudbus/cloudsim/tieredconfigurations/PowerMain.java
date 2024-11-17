1f87d488affd328800ab6043eeb14a528f423d11
Raoul <6421655+Raove@users.noreply.github.com>
2024-11-16 19:41:47 -0800

    Update PowerMain.java

--- modules/cloudsim-green-project/src/main/java/org/cloudbus/cloudsim/tieredconfigurations/PowerMain.java
+++ modules/cloudsim-green-project/src/main/java/org/cloudbus/cloudsim/tieredconfigurations/PowerMain.java
@@ -2,146 +2,116 @@ package org.cloudbus.cloudsim.tieredconfigurations;
 
 import org.cloudbus.cloudsim.*;
 import org.cloudbus.cloudsim.core.CloudSim;
-import org.cloudbus.cloudsim.workload.CloudletDataParser;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class PowerMain {
     private static List<Cloudlet> cloudletList;
     private static List<Vm> vmList;
-    private static RealTimeSimulationManager simulationManager;
+    private static Datacenter highResDatacenter;
+    private static Datacenter mediumResDatacenter;
+    private static Datacenter lowResDatacenter;
 
     public static void main(String[] args) {
         try {
-            // First step: Initialize the CloudSim package.
+            // Step 1: Initialize CloudSim
             int numUsers = 1;
             Calendar calendar = Calendar.getInstance();
-            boolean trace_flag = false;
+            boolean traceFlag = false;
+            CloudSim.init(numUsers, calendar, traceFlag);
 
-            CloudSim.init(numUsers, calendar, trace_flag);
+            // Step 2: Load fossil-free percentages
+            Queue<Double> fossilFreePercentages = loadFossilFreePercentages(
+                    "modules/cloudsim-green-project/src/main/java/org/cloudbus/cloudsim/energyapi/powerBreakdownData.csv"
+            );
+            System.out.println(fossilFreePercentages);
+            PowerData initialPowerData = new PowerData(fossilFreePercentages.poll(), 0);
 
-            // Example power data, in real scenario this will come from the API
-            PowerData initialPowerData = new PowerData();
-            simulationManager = new RealTimeSimulationManager("SimulationTimeManager",initialPowerData);
+            // Step 3: Create fixed datacenters
+            highResDatacenter = DatacenterFactory.createHighResourceDatacenter("High_Resource_Datacenter");
+            mediumResDatacenter = DatacenterFactory.createMediumResourceDatacenter("Medium_Resource_Datacenter");
+            lowResDatacenter = DatacenterFactory.createLowResourceDatacenter("Low_Resource_Datacenter");
 
-            // Second step: Create Datacenters based on fossil fuel percentage
-            Datacenter selectedDatacenter = selectDatacenterBasedOnPowerData(initialPowerData);
-            CloudSim.addEntity(simulationManager);
-            // simulationManager.startRealTimeUpdates();
-
-            // Third step: Create Broker
+            // Step 4: Create broker
             DatacenterBroker broker = createBroker();
             int brokerId = broker.getId();
 
-            // Fourth step: Create one virtual machine
+            // Step 5: Create VMs and Cloudlets
             vmList = new ArrayList<>();
-            // List<Vm> vmList = createVMs();
-            // List<Cloudlet> cloudletList = createCloudlets();
-
-            // create VM and add vm to the vmList
             vmList.add(createVM(brokerId, 0));
             vmList.add(createVM(brokerId, 1));
-
-            // submit vm list to the broker
             broker.submitGuestList(vmList);
 
-            // Fifth step: Create one Cloudlet
             cloudletList = new ArrayList<>();
-
-            // add the cloudlet to the list ######### To be replaced ########
             int id = 0;
-            cloudletList.add(createCloudlet(id, brokerId, 0, 1000000));
-            id++;
-            cloudletList.add(createCloudlet(id, brokerId, 0, 10000000));
-            id++;
-            cloudletList.add(createCloudlet(id, brokerId, 0, 100000000));
-            id++;
-            cloudletList.add(createCloudlet(id, brokerId, 1, 1000000));
-            id++;
-            cloudletList.add(createCloudlet(id, brokerId, 1, 10000000));
-            id++;
-            cloudletList.add(createCloudlet(id, brokerId, 1, 100000000));
-
-            //######### To be replaced ########
-            //cloudletList = CloudletDataParser.parseCloudletData(filePath);
-
-
-            // submit cloudlet list to the broker
+            cloudletList.add(createCloudlet(id++, brokerId, 0, 1000000));
+            cloudletList.add(createCloudlet(id++, brokerId, 0, 10000000));
+            cloudletList.add(createCloudlet(id++, brokerId, 0, 100000000));
+            cloudletList.add(createCloudlet(id++, brokerId, 1, 1000000));
+            cloudletList.add(createCloudlet(id++, brokerId, 1, 10000000));
+            cloudletList.add(createCloudlet(id++, brokerId, 1, 100000000));
             broker.submitCloudletList(cloudletList);
 
-            // Set the amount of time to simulate here, it is in seconds, but it doesn't run for that time, it
-            // simulates that amount of time instead, so 3600 is 1 hour, but your code will run in an instant
-//            CloudSim.terminateSimulation(84000);
+            // Step 6: Simulate for each hour
+            Runnable monitor = () -> {
+                double temp = 0;
+                double hour = 0;
+                String tempName = "";
+                while (CloudSim.running()) {
+                    if (CloudSim.clock() <= temp) {
+//                        System.out.println("Clock: " + CloudSim.clock());
+                        if(!fossilFreePercentages.isEmpty() && temp/3600 > hour) {
+                            initialPowerData.setFossilFreePercentage(fossilFreePercentages.poll());
+                            hour += 1;
+                        }
+                        initialPowerData.setFossilFreePercentage(initialPowerData.getFossilFreePercentage());
+                        Datacenter selectedDatacenter = selectDatacenterBasedOnPowerData(initialPowerData);
+                        if (!selectedDatacenter.getName().equals(tempName)) {
+                            System.out.println(" ------- Hour: " + temp/3600 + " Update Datacenter: ("
+                                    + selectedDatacenter.getName() + ") Simulating based on fossil-free percentage: "
+                                    + initialPowerData.getFossilFreePercentage());
+                            tempName = selectedDatacenter.getName();
+                        }
+                        else {
+                            System.out.println(" ------- Hour: " + temp/3600 + " No Datacenter change based on " +
+                                    "fossil-free percentage: " + initialPowerData.getFossilFreePercentage());
+                        }
+                        temp += 1800;
+//                        if(temp > CloudSim.clock()) {temp = CloudSim.clock();}
+                    }
+                }
+            };
 
-            // Sixth step: Starts the simulation
+            new Thread(monitor).start();
+
+            // Step 7: Start Simulation
             CloudSim.startSimulation();
 
             CloudSim.stopSimulation();
 
-            // Final step: Print results when simulation is over
-            List<Cloudlet> resultList = broker.getCloudletReceivedList();
-            printCloudletResults(resultList);
-
-//            simulationManager.stopRealTimeUpdates();
+            // Step 8: Print Results
+            printCloudletResults(broker.getCloudletReceivedList());
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static Datacenter selectDatacenterBasedOnPowerData(PowerData powerData) throws Exception {
+    private static Datacenter selectDatacenterBasedOnPowerData(PowerData powerData) {
         double fossilFreePercentage = powerData.getFossilFreePercentage();
-        Datacenter highResDatacenter = DatacenterFactory.createHighResourceDatacenter("High_Resource_Datacenter");
-        Datacenter mediumResDatacenter = DatacenterFactory.createMediumResourceDatacenter("Medium_Resource_Datacenter");
-        Datacenter lowResDatacenter = DatacenterFactory.createLowResourceDatacenter("Low_Resource_Datacenter");
-
         if (fossilFreePercentage > 70) {
-            System.out.println("Selecting High Resource Datacenter...");
             return highResDatacenter;
         } else if (fossilFreePercentage > 35) {
-            System.out.println("Selecting Medium Resource Datacenter...");
             return mediumResDatacenter;
         } else {
-            System.out.println("Selecting Low Resource Datacenter...");
             return lowResDatacenter;
-        }
-    }
-
-    //same as the one above, except this one creates the datacenters only when needed
-//    public static Datacenter selectDatacenterBasedOnPowerData(PowerData powerData) throws Exception {
-//        double fossilFreePercentage = powerData.getFossilFreePercentage();
-//
-//        if (fossilFreePercentage > 70) {
-//            System.out.println("Selecting High Resource Datacenter...");
-//            return DatacenterFactory.createHighResourceDatacenter("High_Resource_Datacenter");
-//        } else if (fossilFreePercentage > 35) {
-//            System.out.println("Selecting Medium Resource Datacenter...");
-//            return DatacenterFactory.createMediumResourceDatacenter("Medium_Resource_Datacenter");
-//        } else {
-//            System.out.println("Selecting Low Resource Datacenter...");
-//            return DatacenterFactory.createLowResourceDatacenter("Low_Resource_Datacenter");
-//        }
-//    }
-
-    public static void printCloudletResults(List<Cloudlet> cloudlets) {
-        String indent = "    ";
-        System.out.println("========== OUTPUT ==========");
-        System.out.println("Cloudlet ID" + indent + "STATUS" + indent + "Data Center ID" + indent + "VM ID" + indent
-                + "Time" + indent + "Start Time" + indent + "Finish Time" + indent + "Memory Utilization");
-
-        for (Cloudlet cloudlet : cloudlets) {
-            System.out.print(indent + cloudlet.getCloudletId() + indent + indent + "  ");
-
-            if (cloudlet.getStatus() == Cloudlet.CloudletStatus.SUCCESS) {
-                UtilizationModel memoryUtilizationModel = cloudlet.getUtilizationModelRam();
-                double memoryUtilization = memoryUtilizationModel.getUtilization(CloudSim.clock());
-                System.out.println("SUCCESS" + indent + indent + cloudlet.getResourceId() + indent + indent
-                        + indent + cloudlet.getVmId() + indent + indent + Double.parseDouble(String.format("%.3f",cloudlet.getActualCPUTime())) + indent + indent
-                        + cloudlet.getExecStartTime() + indent + indent + Double.parseDouble(String.format("%.3f",cloudlet.getFinishTime()))
-                        + indent + indent + Double.parseDouble(String.format("%.3f",memoryUtilization)));
-            }
         }
     }
 
@@ -154,36 +124,54 @@ public class PowerMain {
         String vmm = "Xen"; // VMM name
 
         // Create the VM
-        Vm vm = new Vm(vmid, brokerId, mips, pesNumber, ram, bw, size, vmm, new CloudletSchedulerTimeShared());
-
-        return vm;
+        return new Vm(vmid, brokerId, mips, pesNumber, ram, bw, size, vmm, new CloudletSchedulerTimeShared());
     }
 
     public static Cloudlet createCloudlet(int id, int brokerId, int vmid, long length) {
-        int cloudletId = id;
-        // Cloudlet properties
-//        long length = 400000;
         long fileSize = 300;
         long outputSize = 300;
         int pesNumber = 1;
-        // Full utilization model for CPU, RAM, and bandwidth
         UtilizationModel utilizationModel = new UtilizationModelFull();
 
-        Cloudlet cloudlet = new Cloudlet(cloudletId, length, pesNumber, fileSize, outputSize, utilizationModel, utilizationModel, utilizationModel);
-        cloudlet.setUserId(brokerId); // Set the user ID for the cloudlet
+        Cloudlet cloudlet = new Cloudlet(id, length, pesNumber, fileSize, outputSize, utilizationModel, utilizationModel, utilizationModel);
+        cloudlet.setUserId(brokerId);
         cloudlet.setGuestId(vmid);
-
         return cloudlet;
     }
 
-    private static DatacenterBroker createBroker() {
-        DatacenterBroker broker = null;
-        try {
-            broker = new DatacenterBroker("Broker");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+    private static void printCloudletResults(List<Cloudlet> cloudlets) {
+        System.out.println("========== OUTPUT ==========");
+        System.out.println("Cloudlet ID    STATUS    Datacenter ID    VM ID    Time    Start Time    Finish Time");
+        for (Cloudlet cloudlet : cloudlets) {
+            if (cloudlet.getStatus() == Cloudlet.CloudletStatus.SUCCESS) {
+                System.out.printf("%10d    SUCCESS    %12d    %5d    %4.2f    %9.2f    %10.2f%n",
+                        cloudlet.getCloudletId(), cloudlet.getResourceId(), cloudlet.getVmId(),
+                        cloudlet.getActualCPUTime(), cloudlet.getExecStartTime(), cloudlet.getFinishTime());
+            }
         }
-        return broker;
+    }
+
+    private static DatacenterBroker createBroker() throws Exception {
+        return new DatacenterBroker("Broker");
+    }
+
+    public static Queue<Double> loadFossilFreePercentages(String filePath) {
+        Queue<Double> percentages = new LinkedList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            boolean isHeader = true;
+            while ((line = reader.readLine()) != null) {
+                if (isHeader) {
+                    isHeader = false;
+                    continue;
+                }
+                String[] columns = line.split(",");
+                double fossilFreePercentage = Double.parseDouble(columns[28].trim());
+                percentages.add(fossilFreePercentage);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return percentages;
     }
 }
